b694c0e10671c2a75bfa3c11f350a60104e705f6
khaan <nguyenkhaan2006@gmail.com>
2026-03-13 16:29:29 +0700

    style: note to fix the db

---
+++ src/modules/voucher/voucher.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
+
+@Controller("voucher") 
+export class VoucherController 
+{
+    @Get() 
+    async getAllVouchers() 
+    {
+
+    } 
+    @Post() 
+    async createVoucher() 
+    {
+
+    } 
+
+    @Get("/:voucherId") 
+    async getVoucherById(@Param("voucherId" , ParseIntPipe) voucherId : number) 
+    {
+        return voucherId 
+    }
+}
