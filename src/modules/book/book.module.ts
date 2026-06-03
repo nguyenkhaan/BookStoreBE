4f99e478b95a701bbc11e5585fcc06e509a46172
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-06-03 07:00:58 +0700

    fix: fix missing information error

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -3,9 +3,10 @@ import { BookController } from './book.controller';
 import { AuthModule } from '../auth/auth.module';
 import { BookService } from './book.service';
 import { InventoryModule } from '../inventory/inventory.module';
+import { EmailModule } from '../email/email.module';
 
 @Module({
-	imports: [AuthModule, InventoryModule],
+	imports: [AuthModule, InventoryModule, EmailModule],
 	exports: [],
 	controllers: [BookController],
 	providers: [BookService],
